a6b8a99731cbf7244d0e301840574108f084b2f3
peachananr <peachananr@gmail.com>
2013-09-25 22:19:05 +0700

    Initial release

---
+++ Demo/jquery.immersive-slider.js
@@ -0,0 +1,253 @@
+/* ===========================================================
+ * jquery-immersive-slider.js v1
+ * ===========================================================
+ * Copyright 2013 Pete Rojwongsuriya.
+ * http://www.thepetedesign.com
+ *
+ * Create an immersive slider that changes the 
+ * the whole container to match the viewing slide
+ *
+ * https://github.com/peachananr/immersive-slider
+ *
+ * ========================================================== */
+
+!function($){
+  
+  var defaults = {
+    animation: "bounce",
+    slideSelector: ".slide",
+    container: ".main",
+    cssBlur: false,
+    pagination: true
+	};
+  
+  /*------------------------------------------------*/
+	/*  Credit: Eike Send for the awesome swipe event */    
+	/*------------------------------------------------*/
+	
+	$.fn.swipeEvents = function() {
+      return this.each(function() {
+
+        var startX,
+            startY,
+            $this = $(this);
+
+        $this.bind('touchstart', touchstart);
+
+        function touchstart(event) {
+          var touches = event.originalEvent.touches;
+          if (touches && touches.length) {
+            startX = touches[0].pageX;
+            startY = touches[0].pageY;
+            $this.bind('touchmove', touchmove);
+          }
+          event.preventDefault();
+        }
+
+        function touchmove(event) {
+          var touches = event.originalEvent.touches;
+          if (touches && touches.length) {
+            var deltaX = startX - touches[0].pageX;
+            var deltaY = startY - touches[0].pageY;
+
+            if (deltaX >= 50) {
+              $this.trigger("swipeLeft");
+            }
+            if (deltaX <= -50) {
+              $this.trigger("swipeRight");
+            }
+            if (deltaY >= 50) {
+              $this.trigger("swipeUp");
+            }
+            if (deltaY <= -50) {
+              $this.trigger("swipeDown");
+            }
+            if (Math.abs(deltaX) >= 50 || Math.abs(deltaY) >= 50) {
+              $this.unbind('touchmove', touchmove);
+            }
+          }
+          event.preventDefault();
+        }
+
+      });
+    };
+  
+  $.fn.transformSlider = function(settings, pos) {
+    var el = $(this)
+    switch(settings.animation) {
+      case 'slide':
+        el.addClass("ease").css({
+          "-webkit-transform": "translate3d(" + pos + "%, 0, 0)", 
+          "-moz-transform": "translate3d(" + pos + "%, 0, 0)", 
+          "-ms-transform": "translate3d(" + pos + "%, 0, 0)", 
+          "transform": "translate3d(" + pos + "%, 0, 0)"
+        });
+      break;
+      case 'slideUp':
+        el.addClass("ease").css({
+          "-webkit-transform": "translate3d(0, " + pos + "%, 0)", 
+          "-moz-transform": "translate3d(0, " + pos + "%, 0)", 
+          "-ms-transform": "translate3d(0, " + pos + "%, 0)", 
+          "transform": "translate3d(0, " + pos + "%, 0)"
+        });
+      break;
+      case 'bounce':
+        el.addClass("bounce").css({
+          "-webkit-transform": "translate3d(" + pos + "%, 0, 0)", 
+          "-moz-transform": "translate3d(" + pos + "%, 0, 0)", 
+          "-ms-transform": "translate3d(" + pos + "%, 0, 0)", 
+          "transform": "translate3d(" + pos + "%, 0, 0)"
+        });
+      break;
+      case 'bounceUp':
+        el.addClass("bounce").css({
+          "-webkit-transform": "translate3d(0, " + pos + "%, 0)", 
+          "-moz-transform": "translate3d(0, " + pos + "%, 0)", 
+          "-ms-transform": "translate3d(0, " + pos + "%, 0)", 
+          "transform": "translate3d(0, " + pos + "%, 0)"
+        });
+      break;
+      case 'fade':
+        el.addClass("no-animation").fadeOut("slow", function() {
+          el.css({
+            "-webkit-transform": "translate3d(" + pos + "%, 0, 0)", 
+            "-moz-transform": "translate3d(" + pos + "%, 0, 0)", 
+            "-ms-transform": "translate3d(" + pos + "%, 0, 0)", 
+            "transform": "translate3d(" + pos + "%, 0, 0)"
+          }).fadeIn("slow");
+        });
+        
+      break;
+    }
+  }
+  
+  $.fn.positionSlides = function(settings, index) {
+    var el = $(this);
+    if (settings.animation == "slideUp" || settings.animation == "bounceUp") {
+      el.css({
+        top: (index * 100) + "%"
+      });
+    }else {
+      el.css({
+        left: (index * 100) + "%"
+      });
+    }
+  }
+  
+  
+  
+  $.fn.immersive_slider = function(options){
+    var settings = $.extend({}, defaults, options),
+        el = $(this),
+        cssblur = "",
+        pagination = "";
+        
+    // Add all the gs sepecific classes  
+    el.addClass("immersive_slider")
+    el.find(settings.slideSelector).addClass("is-slide");
+    
+    // Use CSS to blur the first image the plugin found automatically 
+    if (settings.cssBlur == true) {
+      el.find(".is-slide img:first-child").each(function( index ) {
+        var activeclass = ""
+        if(index == 0) activeclass = "active"
+        var img = $(this);
+        
+        $(settings.container).addClass("is-container").prepend("<div id='slide_" + (index + 1) + "_bg' class='is-background gs_cssblur " + activeclass + "'>" + img.clone().wrap("<div />").parent().html() + "</div>")
+        $("#slide_" + (index + 1) + "_bg").positionSlides(settings, index)
+      });
+    } else {
+      el.find(".is-slide").each(function( index ) {
+        var activeclass = ""
+        if(index == 0) activeclass = "active"
+        var img = "<img src='"+ $(this).data("blurred") +"'>";
+        
+        $(settings.container).addClass("is-container").prepend("<div id='slide_" + (index + 1) + "_bg' class='is-background " + activeclass + "'>" + img + "</div>")
+        $("#slide_" + (index + 1) + "_bg").positionSlides(settings, index)
+      });
+    }
+    
+    
+    $(settings.container).find(".is-background").wrapAll( "<div class='is-bg-overflow' />");
+    el.find(".is-slide").wrapAll( "<div class='is-overflow' />");
+    el.find(".is-slide").each(function( index ) {
+      var activeclass = ""
+      if(index == 0) activeclass = "active"
+      $(this).attr("id","slide_" + (index + 1)).addClass(activeclass)
+      $(this).positionSlides(settings, index)
+      if(settings.pagination == true) {
+        pagination += "<li><a class='is-page " + activeclass + "' href='#slide_" + (index + 1) + "'></a></li>"
+      }
+    });
+    
+    $("<ul class='is-pagination'>"+pagination+"</ul>").appendTo(el)
+    
+    if(settings.pagination == true)  {
+      $(".is-pagination li a").click(function (){
+        var page_index = $(this).attr("href");
+        
+        if (!$(this).hasClass("active")) {
+          el.moveSlider(settings, page_index)
+        }
+        return false
+      });
+    }
+   $(".is-next").click(function() {
+     el.moveNext();
+     return false;
+   });
+   $(".is-prev").click(function() {
+     el.movePrev();
+     return false;
+   });
+    
+    $.fn.moveSlider = function(settings, page_index) {
+      var el = $(this),
+          current = el.find(".is-slide.active"),
+          next = el.find(".is-slide" + page_index),
+          bg_current = $(settings.container).find(".is-background.active"),
+          bg_next = $(settings.container).find(".is-background" + page_index + "_bg");
+      if(next) {
+        current.removeClass("active")
+        next.addClass("active")
+        
+        bg_current.removeClass("active")
+        bg_next.addClass("active")
+        $(".is-pagination li a" + ".active").removeClass("active");
+        $(".is-pagination li a" + "[href='" + (page_index) + "']").addClass("active");
+      }
+      pos = ((page_index.replace('#slide_','') - 1) * 100) * -1;
+      el.find(".is-overflow").transformSlider(settings, pos);
+      $(settings.container).find(".is-bg-overflow").transformSlider(settings, pos);
+    }
+    
+    $.fn.moveNext = function() {
+      var el = $(this),
+        total = el.find(settings.slideSelector).length + 1,
+        page_index_number = parseInt($(this).find(".is-slide.active").attr("id").replace('slide_','')) + 1;
+        console.log()
+      if(page_index_number < total) {
+        el.moveSlider(settings, "#slide_" + page_index_number)
+      }
+    }
+    
+    $.fn.movePrev = function() {
+      var el = $(this),
+        total = el.find(settings.slideSelector).length + 1,
+        page_index_number = parseInt($(this).find(".is-slide.active").attr("id").replace('slide_','')) - 1;
+      if(page_index_number <= total && page_index_number > 0) {
+        el.moveSlider(settings, "#slide_" + page_index_number)
+      }
+    }
+    
+    el.swipeEvents().bind("swipeRight",  function(){ 
+      el.movePrev();
+    }).bind("swipeLeft", function(){ 
+      el.moveNext(); 
+    });
+  
+  }
+  
+}(window.jQuery);
+
+
--- README.md
+++ README.md
@@ -1,4 +1,96 @@
-immersive-slider
-================
+#Immersive Slider by Pete R.
+Create an immersive slider that changes the whole container to match the viewing slide
+Created by [Pete R.](http://www.thepetedesign.com), Founder of [BucketListly](http://www.bucketlistly.com)
 
-Create an immersive slider experience like no others.
+[![Immersive Slider](http://www.thepetedesign.com/images/immersive_slider_image.png "Immersive Slider")](http://www.thepetedesign.com/demos/immersive_slider_demo.html)
+
+## Demo
+[View demo](http://www.thepetedesign.com/demos/immersive_slider_demo.html)
+
+## Compatibility
+Modern browsers such as Chrome, Firefox, and Safari on both desktop and smartphones have been tested. Not tested on IE.
+
+## Basic Usage
+Immersive Slider let you create a unique immersive slider experience that changes the whole container to match the viewing slide like you see at the [Google's TV website](http://www.google.com/tv/). The plugin will let you assign background images per slide without you doing all the hard work. 
+
+To add this to your website, simply include the latest jQuery library together with `jquery.immersive-slider.js`, and `immersive-slider.css` into your document's `<head>` and create an HTML markup as follows:
+
+````html
+<div id="immersive_slider">
+  <div class="slide" data-blurred="<<background-image-url>>">
+    ...
+  </div>
+  <div class="slide" data-blurred="<<background-image-url>>">
+    ...
+  </div>
+  
+  ...
+
+  <a href="#" class="is-prev">&laquo;</a>
+  <a href="#" class="is-next">&raquo;</a>
+</div>
+````
+Make sure you change all the `<<background-image-url>>` occurrences to the image path you wish the slider to show when that slide is active. Feel free to remove the navigational buttons if you don't need it. Once that is done, simply call the script like this:
+ 
+````javascript
+$("#immersive_slider").immersive_slider({
+  animation: "bounce", // As usual, you can change the animation to these: slide (default), bounce, fade, slideUp, and bounceUp
+  slideSelector: ".slide", // This option will let you assign custom selector for each slides in case .slide is already taken
+  container: ".main", // This option lets you define the container of which the background will appear. Make sure the slider is inside this container as well.
+  cssBlur: false, // Experimental: In case you don't want to keep adding new data-blurred attributes, trigger this to true and it will generate the blur image on the fly (more info below).
+  pagination: true // Toggle this to false if you don't want a pagination
+});
+````
+And that's all you have to do to get this plugin up and running on your website. Pretty easy right?
+
+## Experimental Feature: CSSBlur
+The plugin is capable of blurring the first image it finds in each slide, blow them up and use it as a background without you having to manually set each slide a background image. This is done using the CSS3 style called Filter which only works on Chrome, and maybe a performance hog. Use it at your own risk.
+
+To do this simply toggle the cssBlur option to true like this:
+
+````javascript
+$("#immersive_slider").immersive_slider({
+  cssBlur: true
+});
+```` 
+
+and make sure the images you want to blow up as a background are in each slides like this:
+
+````html
+<div id="immersive_slider">
+  <div class="slide">
+    <img src="<<background-image-url>>">
+    ...
+  </div>
+  <div class="slide">
+    <img src="<<background-image-url>>">
+    ...
+  </div>
+  ...
+</div>
+````
+
+The script will grab this first image, apply the CSS filter and blow them up with CSS filling the container for you. This is great when you already have an existing slider and you don't want to add a new attribute for the background.
+
+## Public Methods
+You can also trigger the slider to move programmatically as well:
+
+### $.fn.moveNext()
+This method allows you to move the slider to the next one.
+
+````javascript
+  $("#immersive_slider").mmoveNextoveUp();
+````
+
+### $.fn.movePrev()
+This method allows you to move the slider to the previous one.
+
+````javascript
+  $("#immersive_slider").movePrev();
+````
+
+If you want to see more of my plugins, visit [The Pete Design](http://www.thepetedesign.com/#design), or follow me on [Twitter](http://www.twitter.com/peachananr) and [Github](http://www.github.com/peachananr).
+
+## Other Resources
+- Tutorial (Coming Soon)
+- [Eike Send's jQuery Swipe Events](https://github.com/eikes/jquery.swipe-events.js)
